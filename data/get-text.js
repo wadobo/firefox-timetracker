ce5b661d3c72597a443aaf79c505af6501a46ee4
Javier Aguirre <contacto@javaguirre.net>
2013-03-05 14:28:29 +0100

    Creating the timer, changing favicon

--- data/get-text.js
+++ data/get-text.js
@@ -3,36 +3,40 @@ var startButton = document.getElementById("start-button");
 var stopButton = document.getElementById("stop-button");
 var commitButton = document.getElementById("commit-button");
 var timer = document.getElementById("timer");
+
 timer.obj = null;
-
-timer.start = function(date) {
-    timer.obj = date;
-};
-
-timer.stop = function() {
-    timer.obj = null;
-};
+timer.lasttime = 0;
 
 var updateTimer = function() {
   var date = new Date();
   var hours = 0;
   var minutes = 0;
 
-  if(date.getHours() < timer.obj.getHours()) {
-    hours = date.getHours() - timer.obj.getHours();
-  }
-  if(date.getMinutes() < timer.obj.getHours()) {
-    minutes = date.getMinutes() - timer.obj.getMinutes();
-  }
+  if(timer.obj) {
+    currenttime = date.getTime();
+    totaltime = (currenttime - timer.lasttime)/(1000*3600);
 
-  timer.textContent = hours +
-                      '.' + minutes +
-                      'h to commit';
+    timer.textContent = totaltime.toFixed(2) +
+                        'h to commit';
+  }
+};
+
+timer.start = function(date) {
+  timer.obj = date;
+  timer.lasttime = date.getTime();
+  startButton.style.display = 'none';
+  stopButton.style.display = 'block';
+};
+
+timer.stop = function() {
+  timer.obj = null;
+  timer.lasttime = 0;
+  startButton.style.display = 'block';
+  stopButton.style.display = 'none';
 };
 
 textArea.addEventListener('keyup', function onkeyup(event) {
-  if (event.keyCode == 13) {
-    // Remove the newline.
+  if (event.keyCode === 13) {
     text = textArea.value.replace(/(\r\n|\n|\r)/gm,"");
     self.port.emit("text-entered", text);
     textArea.value = '';
@@ -49,7 +53,7 @@ startButton.addEventListener('click', function onclickevent(event) {
                    ':' + date.getMinutes() +
                    ' start ' + project + '\n';
   timer.start(date);
-  updateTimer();
+  setInterval(updateTimer, 10000);
 });
 
 stopButton.addEventListener('click', function onclickevent(event) {
